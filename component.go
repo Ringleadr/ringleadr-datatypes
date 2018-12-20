f07c0a5fa97ad8a849b448b8e2b304d6b10c0bc7
Ed <edward.dobson3@ntlworld.com>
2018-12-20 12:30:57 +0000

    Add storage to components

--- component.go
+++ component.go
@@ -4,4 +4,8 @@ type Component struct {
 	Name     string `json:"name"`
 	Image    string `json:"image" binding:"required"`
 	Replicas int    `json:"replicas"`
+	Storage  struct {
+		Name      string `json:"name"`
+		MountPath string `json:"mount_path"`
+	} `json:"storage"`
 }
