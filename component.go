9adc4c96cb5f664513e31eabac1dea3df3168464
Ed <edward.dobson3@ntlworld.com>
2018-12-20 13:27:34 +0000

    Trying something else

--- component.go
+++ component.go
@@ -1,11 +1,13 @@
 package Datatypes
 
 type Component struct {
-	Name     string `json:"name"`
-	Image    string `json:"image" binding:"required"`
-	Replicas int    `json:"replicas"`
-	Storage  []struct {
-		Name      string `json:"name"`
-		MountPath string `json:"mount_path"`
-	} `json:"storage"`
+	Name     string      `json:"name"`
+	Image    string      `json:"image" binding:"required"`
+	Replicas int         `json:"replicas"`
+	Storage  []*storeage `json:"storage"`
+}
+
+type storeage struct {
+	Name      string `json:"name"`
+	MountPath string `json:"mount_path"`
 }
