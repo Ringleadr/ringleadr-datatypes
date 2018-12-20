f35e380f79b97eb09b0a445c21c56935ebfde3aa
Ed <edward.dobson3@ntlworld.com>
2018-12-20 12:41:27 +0000

    Allow an array of storage

--- component.go
+++ component.go
@@ -4,7 +4,7 @@ type Component struct {
 	Name     string `json:"name"`
 	Image    string `json:"image" binding:"required"`
 	Replicas int    `json:"replicas"`
-	Storage  struct {
+	Storage  []struct {
 		Name      string `json:"name"`
 		MountPath string `json:"mount_path"`
 	} `json:"storage"`
