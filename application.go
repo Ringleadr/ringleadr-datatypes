d06a9cf55c9940de75edf69f7c8ba411fc37eccd
Ed <edward.dobson3@ntlworld.com>
2018-12-30 11:42:55 +0000

    Add copies to top level application definition Fix storage typo

--- application.go
+++ application.go
@@ -2,5 +2,6 @@ package Datatypes
 
 type Application struct {
 	Name       string       `json:"name" binding:"required"`
+	Copies     int          `json:"copies"`
 	Components []*Component `json:"components" binding:"required"`
 }
--- component.go
+++ component.go
@@ -1,13 +1,13 @@
 package Datatypes
 
 type Component struct {
-	Name     string      `json:"name"`
-	Image    string      `json:"image" binding:"required"`
-	Replicas int         `json:"replicas"`
-	Storage  []*storeage `json:"storage"`
+	Name     string     `json:"name"`
+	Image    string     `json:"image" binding:"required"`
+	Replicas int        `json:"replicas"`
+	Storage  []*storage `json:"storage"`
 }
 
-type storeage struct {
+type storage struct {
 	Name      string `json:"name"`
 	MountPath string `json:"mount_path"`
 }
