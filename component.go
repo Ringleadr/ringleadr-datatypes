1fafcc7830bda4a4f74c951dc4ddb5a8e84e8d0a
Ed <edward.dobson3@ntlworld.com>
2019-01-08 16:40:41 +0000

    Add ports to component

--- component.go
+++ component.go
@@ -1,10 +1,11 @@
 package Datatypes
 
 type Component struct {
-	Name     string     `json:"name"`
-	Image    string     `json:"image" binding:"required"`
-	Replicas int        `json:"replicas"`
-	Storage  []*storage `json:"storage"`
+	Name     string            `json:"name"`
+	Image    string            `json:"image" binding:"required"`
+	Replicas int               `json:"replicas"`
+	Storage  []*storage        `json:"storage"`
+	Ports    map[string]string `json:"ports"`
 }
 
 type storage struct {
