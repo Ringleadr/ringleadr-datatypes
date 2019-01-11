d9452a19eb9f988d8876c32f644fd49a6a4e2f6b
Ed <edward.dobson3@ntlworld.com>
2019-01-11 16:52:38 +0000

    Add networks to application datatype

--- application.go
+++ application.go
@@ -4,4 +4,5 @@ type Application struct {
 	Name       string       `json:"name" binding:"required"`
 	Copies     int          `json:"copies"`
 	Components []*Component `json:"components" binding:"required"`
+	Networks   []string     `json:"networks"`
 }
