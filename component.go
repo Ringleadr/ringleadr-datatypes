dea9a7da0ebd5b36af603d6b2696105cb062edcb
Ed <edward.dobson3@ntlworld.com>
2019-01-24 16:37:16 +0000

    Add scale threshold

--- component.go
+++ component.go
@@ -1,13 +1,14 @@
 package Datatypes
 
 type Component struct {
-	Name     string            `json:"name"`
-	Image    string            `json:"image" binding:"required"`
-	Replicas int               `json:"replicas"`
-	Storage  []*storage        `json:"storage"`
-	Ports    map[string]string `json:"ports"`
-	Env      []string          `json:"env"`
-	Status   string            `json:"status"`
+	Name           string            `json:"name"`
+	Image          string            `json:"image" binding:"required"`
+	Replicas       int               `json:"replicas"`
+	Storage        []*storage        `json:"storage"`
+	Ports          map[string]string `json:"ports"`
+	Env            []string          `json:"env"`
+	Status         string            `json:"status"`
+	ScaleThreshold float64           `json:"scale_threshold"`
 }
 
 type storage struct {
