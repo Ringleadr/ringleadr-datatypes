18a5a9a252f769fc71b55d0962da2d9166eb522a
Ed <edward.dobson3@ntlworld.com>
2019-02-15 15:19:02 +0000

    Node stats

--- node.go
+++ node.go
@@ -5,3 +5,10 @@ type Node struct {
 	Address string `json:"address" binding:"required"`
 	Active  bool   `json:"active"`
 }
+
+type NodeStats struct {
+	TotalMem   float64 `json:"total_mem"`
+	UsedMem    float64 `json:"used_mem"`
+	Cpus       int     `json:"cpus"`
+	CpuPercent float64 `json:"cpu_percent"`
+}
