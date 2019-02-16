23cdc58dfe3ff2868f04c64140da8a3d069982a9
Ed <edward.dobson3@ntlworld.com>
2019-02-16 13:45:23 +0000

    Add mem percent to stats

--- node.go
+++ node.go
@@ -12,10 +12,11 @@ type NodeStatsEntry struct {
 }
 
 type NodeStats struct {
-	TotalMem      uint64  `json:"total_mem" bson:"total_mem"`
-	UsedMem       uint64  `json:"used_mem" bson:"used_mem"`
-	Cpus          int     `json:"cpus" bson:"cpus"`
-	CpuPercent    float64 `json:"cpu_percent" bson:"cpu_percent"`
-	NumContainers int     `json:"num_containers" bson:"num_containers"`
-	Timestamp     int64   `json:"timestamp" bson:"timestamp"`
+	TotalMem       uint64  `json:"total_mem" bson:"total_mem"`
+	UsedMem        uint64  `json:"used_mem" bson:"used_mem"`
+	UsedMemPercent float64 `json:"used_mem_percent" bson:"used_mem_percent"`
+	Cpus           int     `json:"cpus" bson:"cpus"`
+	CpuPercent     float64 `json:"cpu_percent" bson:"cpu_percent"`
+	NumContainers  int     `json:"num_containers" bson:"num_containers"`
+	Timestamp      int64   `json:"timestamp" bson:"timestamp"`
 }
