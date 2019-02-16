12d608e86d38f444989b7fb8ad7189c968793e17
Ed <edward.dobson3@ntlworld.com>
2019-02-16 12:56:49 +0000

    Update node stats definition

--- node.go
+++ node.go
@@ -6,9 +6,16 @@ type Node struct {
 	Active  bool   `json:"active"`
 }
 
+type NodeStatsEntry struct {
+	Name  string      `json:"name" bson:"name"`
+	Stats []NodeStats `json:"stats" bson:"stats"`
+}
+
 type NodeStats struct {
-	TotalMem   uint64  `json:"total_mem"`
-	UsedMem    uint64  `json:"used_mem"`
-	Cpus       int     `json:"cpus"`
-	CpuPercent float64 `json:"cpu_percent"`
+	TotalMem      uint64  `json:"total_mem" bson:"total_mem"`
+	UsedMem       uint64  `json:"used_mem" bson:"used_mem"`
+	Cpus          int     `json:"cpus" bson:"cpus"`
+	CpuPercent    float64 `json:"cpu_percent" bson:"cpu_percent"`
+	NumContainers int     `json:"num_containers" bson:"num_containers"`
+	Timestamp     int64   `json:"timestamp" bson:"timestamp"`
 }
